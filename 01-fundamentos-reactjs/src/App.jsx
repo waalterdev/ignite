5f97822e15bb5c8b60dc905e582cf4840921565c
Sherman Walter <srzprivate@outlook.com>
2025-03-21 11:22:28 -0300

    criado componente header

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from "./components/Header"
 import PostComponent from "./Post"
+
+import './styles.css'
 
 function App() {
   return (
     <div>
+      <Header />
       <PostComponent author="Sherman Walter" content="contect" />
     </div>
   )

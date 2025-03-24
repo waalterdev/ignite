53b7106a9fab06ad5de9bab538678b4c89aca4ce
Sherman Walter <srzprivate@outlook.com>
2025-03-24 15:12:06 -0300

    Estrutura da aplicação - Componente: Post

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,9 +1,9 @@
 import { Header } from "./components/Header"
-import PostComponent from "./Post"
 
 import './global.css'
 import styles from './App.module.css'
 import { Sidebar } from "./components/Sidebar"
+import { Post } from "./components/Post"
 
 function App() {
   return (
@@ -12,13 +12,7 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <PostComponent
-            author="Sherman Walter"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, ex corrupti? Omnis, repudiandae minima ipsa doloremque deleniti dignissimos, sint quas sunt sit hic vero aperiam dolores! Excepturi nisi voluptas accusantium?"
-          />
-          <PostComponent
-            author="Maria Eduarda"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, ex corrupti? Omnis, repudiandae minima ipsa doloremque deleniti dignissimos, sint quas sunt sit hic vero aperiam dolores! Excepturi nisi voluptas accusantium?"
+          <Post 
           />
         </main>
       </div>
--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -7,9 +7,9 @@ export function Sidebar() {
             <img className={styles.cover} src="https://images.unsplash.com/photo-1742387436246-b432a6bfc623?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=100&w=300&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-                <strong>Marília Mendonça</strong>
-                <span>Web Developer</span>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/188369598?v=4" />
+                <strong>Sherman Walter</strong>
+                <span>Fullstack Developer</span>
             </div>
 
             <footer>

758f1d3d7b316b6c4a24f90b67c3a12c7fab3768
Sherman Walter <srzprivate@outlook.com>
2025-03-24 16:59:02 -0300

    Estrutura da aplicação - Componente: Avatar

--- 01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from '@phosphor-icons/react';
 import styles from './Comment.module.css'
+import { Avatar } from './Avatar';
 
 export function Comment() {
     return (
         <div className={styles.comment}>
-            <img src="https://avatars.githubusercontent.com/u/188369598?v=4" />
+            <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/188369598?v=4" />
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
                     <header>

5f65bfe83a1372622d03d8fce218f3a4c4900aba
Sherman Walter <srzprivate@outlook.com>
2025-04-01 09:32:49 -0300

    Os motores do React - Realizando likes nos comentários

--- 01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,11 +1,18 @@
 import { ThumbsUp, Trash } from '@phosphor-icons/react';
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
 export function Comment({ content, onDeleteComment }) {
 
+    const [likeCount, setLikeCount] = useState(0)
+
     function handleDeleteComment() {
         onDeleteComment(content)
+    }
+
+    function handleLike() {
+        setLikeCount(likeCount + 1)
     }
 
     return (
@@ -26,9 +33,9 @@ export function Comment({ content, onDeleteComment }) {
                     <p>{content}</p>
                 </div>
                 <footer>
-                    <button>
+                    <button onClick={handleLike}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
